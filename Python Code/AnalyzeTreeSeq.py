0e56d73ed80e152e9818bff932981b9ef41ac44d
Sohan Alleshwaram <82074892+The-Senate-I-Am@users.noreply.github.com>
2025-10-13 21:16:26 -0500

    Hooray I can now run the whole simulation with a single console command
    TODO: clean up logs and add functionality where all the years have data output at the same time.

--- Python Code/AnalyzeTreeSeq.py
+++ Python Code/AnalyzeTreeSeq.py
@@ -2,59 +2,100 @@ import tskit
 import msprime, pyslim
 import pandas as pd
 import math
+import numpy as np
+import csv
+
+def analyze_tree_sequence():
+    # Load the cluster_data CSV file
+    cluster_data = pd.read_csv("../data/cluster_data.csv")
+
+    assignments = cluster_data['Genome Assignment']
+
+    total_assignments = int(max(assignments.dropna())) 
+
+    genome_indicies = [-1] * (total_assignments+1)
+
+    for i in range(len(assignments)):
+        if math.isnan(assignments[i]) == False:
+            index = int(assignments[i])
+            if genome_indicies[index] == -1:
+                genome_indicies[index] = i
+
+    # Load the tree sequence file
+    ts = tskit.load("../out/simTreeSeq.trees")
 
 
-# Load the cluster_data CSV file
-cluster_data = pd.read_csv("../data/cluster_data.csv")
-
-assignments = cluster_data['Genome Assignment']
-
-total_assignments = int(max(assignments.dropna())) 
-
-genome_indicies = [-1] * (total_assignments+1)
-
-for i in range(len(assignments)):
-    if math.isnan(assignments[i]) == False:
-        index = int(assignments[i])
-        if genome_indicies[index] == -1:
-            genome_indicies[index] = i
-
-# Load the tree sequence file
-ts = tskit.load("../out/simTreeSeq.trees")
-
-ts = pyslim.recapitate(ts, recombination_rate=1e-8, ancestral_Ne=6700)
-#ts = ts.simplify(samples=genome_indicies)
+    diversities = []
+    for idx in genome_indicies:
+        # Get the sample node for this population
+        pop_samples = ts.samples(population=idx)
+        #calculate diversity and append to list
+        pi = ts.diversity([pop_samples])
+        diversities.append(pi[0])
+    diversities = np.array(diversities)
+        
+    print("Diversity (pi) for each genome assignment before recapitation and mutation addition:")
+    print(diversities)
 
 
+    ts = pyslim.recapitate(ts, recombination_rate=1e-8, ancestral_Ne=6700)
+    #ts = ts.simplify(samples=genome_indicies)
+    next_id = pyslim.next_slim_mutation_id(ts)
+    ts = msprime.sim_mutations(
+            ts,
+            rate=1e-8,
+            model=msprime.SLiMMutationModel(type=0, next_id=next_id),
+            keep=True,
+    )
 
-# Calculate diversity for each population in genome_indicies
+    print(f"The tree sequence now has {ts.num_mutations} mutations,\n"
+        f"and mean pairwise nucleotide diversity is {ts.diversity():0.3e}.")
 
 
-diversities = []
-for idx in genome_indicies:
-    # Get the sample node for this population
-    pop_samples = ts.samples(population=idx)
-    #calculate diversity and append to list
-    pi = ts.diversity([pop_samples])
-    diversities.append(pi)
+    # Calculate diversity for each population in genome_indicies
+
+
+    diversities = []
+    for idx in genome_indicies:
+        # Get the sample node for this population
+        pop_samples = ts.samples(population=idx)
+        #calculate diversity and append to list
+        pi = ts.diversity([pop_samples])
+        diversities.append(pi[0])
+    diversities = np.array(diversities)
+        
+    print("Diversity (pi) for each genome assignment:")
+    print(diversities)
+        
+    divergences = []
+
+    for i in range(len(genome_indicies)):
+        divergences.append([])
+        for j in range(len(genome_indicies)):
+            if i == j:
+                divergences[i].append(0)
+            #elif i > j:
+            #    divergences[i].append(divergences[j][i])
+            else:
+                p1 = ts.samples(population=genome_indicies[i])
+                p2 = ts.samples(population=genome_indicies[j])
+                divergence = ts.divergence([p1, p2])
+                divergences[i].append(divergence.item())
+
+    # for divergence in divergences:
+    #     print(divergence)
     
-print("Diversity (pi) for each genome assignment:")
-print(diversities)
     
-divergences = []
-
-for i in range(len(genome_indicies)):
-    divergences.append([])
-    for j in range(len(genome_indicies)):
-        if i == j:
-            divergences[i].append(0)
-        #elif i > j:
-        #    divergences[i].append(divergences[j][i])
-        else:
-            p1 = ts.samples(population=genome_indicies[i])
-            p2 = ts.samples(population=genome_indicies[j])
-            divergence = ts.divergence([p1, p2])
-            divergences[i].append(divergence)
-
-# for divergence in divergences:
-#     print(divergence)
+    #write the data
+    with open("../data/diversitiescsv", "w", newline="") as f:
+        writer = csv.writer(f)
+        for item in diversities:
+            writer.writerow([item])
+        
+    with open("../data/divergences.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(divergences)
+        
+    
+        
+#analyze_tree_sequence()
--- Python Code/Main.py
+++ Python Code/Main.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 import random
 import matplotlib.pyplot as plt
+
 import DataWrappers
 import CollectData
 import GenerateSimulationParams
 import GenerateClusterData
+import AnalyzeTreeSeq
+
+import subprocess
 
 #WARNING: don't run this file in VSCode. Run it in the terminal instead.
 
@@ -13,7 +17,7 @@ def main():
     #Start by reading the data from final_data_for_modeling.csv
     field_data = CollectData.read_csv('../data/final_data_for_modeling.csv')
     
-    #Create clusters from the field data
+    #Ask for input on number of clusters
     num_clusters = int(input("Enter the number of clusters (default 99): ").strip() or 99)
     
     #Cluster the coordinates using KMeans
@@ -22,7 +26,10 @@ def main():
     #Put the data for clusters into a list of Cluster objects
     clusters = GenerateClusterData.populate_cluster_objects(field_data, estimate_data=True)
     
+    #Assign genomes to clusters based on the specifier matrix for a certain year
     GenerateClusterData.assign_genomes_to_clusters(clusters, specifier_matrix="../data/Genetic_Data/specifier_matrix_2023.csv")
+    
+    #TODO: fix logic to do with choosing the genomes for clusters and make sure that all years are done at the same time.
 
     #Generate a distance matrix for the clusters
     distances = GenerateClusterData.create_cluster_distance_matrix(clusters, output_path='../data/cluster_distances.csv')   
@@ -35,6 +42,13 @@ def main():
     
     #Generate migration rates based on the cluster distance matrix
     migration_rates = GenerateSimulationParams.determine_migration_rates(distances, modifier=migration_rates_modifier, output_path='../data/migration_rates.csv')
+    
+    #Run the SLiM simulation to create the tree sequence
+    subprocess.run(['slim', '../SLiM Code/CPBSampleSim.slim'])
+    
+    AnalyzeTreeSeq.analyze_tree_sequence()
+    
+    
     
 
     
